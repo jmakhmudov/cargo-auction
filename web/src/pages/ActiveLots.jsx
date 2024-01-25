58eb0dae1a6453e9cd0312746c4800cde114da75
jmakhmudov <makhmudov@tuta.io>
2024-01-25 16:04:10 +0500

    firebase & filter by country

--- web/src/pages/ActiveLots.jsx
+++ web/src/pages/ActiveLots.jsx
@@ -12,10 +12,22 @@ const ActiveLots = () => {
   const [lots, setLots] = useState([]);
   const [filteredLots, setFilteredLots] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
   const searchLot = (id) => {
     const filtered = lots.filter((lot) => lot.id.toString().includes(id));
     setFilteredLots(filtered);
+  };
+
+  const searchLotByCountry = (country) => {
+    if (selectedCountry === country) {
+      setSelectedCountry(null);
+      setFilteredLots(lots)
+    } else {
+      const filtered = lots.filter((lot) => lot.departure.includes(country));
+      setSelectedCountry(country)
+      setFilteredLots(filtered);
+    }
   };
 
   useEffect(() => {
@@ -35,6 +47,8 @@ const ActiveLots = () => {
       });
   }, []);
 
+  const uniqueCountries = Array.from(new Set(lots.map((lot) => lot.departure.split(',').pop().trim())));
+
   return (
     <PageTemplate title="Актуальные лоты">
       <input
@@ -46,20 +60,33 @@ const ActiveLots = () => {
       {loading && <Loading />}
       {!loading && lots.length === 0 && <Empty />}
       {!loading && lots.length > 0 && (
-        <div className="grid gap-6 pb-20">
-          {filteredLots.length > 0
-            ? filteredLots.map((lot, idx) => (
-              <div key={lot.id}>
-                <LotCard lot={lot} />
-                {idx + 1 !== filteredLots.length ? <hr className="mt-6" /> : <></>}
-              </div>
-            ))
-            : lots.map((lot, idx) => (
-              <div key={lot.id}>
-                <LotCard lot={lot} />
-                {idx + 1 !== lots.length ? <hr className="mt-6" /> : <></>}
+        <div>
+          <section className="flex items-center gap-2 my-2 overflow-x-auto no-scrollbar">
+            {uniqueCountries.map((country) => (
+              <div
+                className={`border-[1.5px] border-gray px-3 py-[2px] rounded-md text-gray text-sm cursor-pointer select-none ${selectedCountry === country ? 'selected' : ''}`}
+                key={country}
+                onClick={(e) => searchLotByCountry(country)}
+              >
+                {country}
               </div>
             ))}
+          </section>
+          <div className="grid gap-6 pb-20">
+            {filteredLots.length > 0
+              ? filteredLots.map((lot, idx) => (
+                <div key={lot.id}>
+                  <LotCard lot={lot} />
+                  {idx + 1 !== filteredLots.length ? <hr className="mt-6" /> : <></>}
+                </div>
+              ))
+              : lots.map((lot, idx) => (
+                <div key={lot.id}>
+                  <LotCard lot={lot} />
+                  {idx + 1 !== lots.length ? <hr className="mt-6" /> : <></>}
+                </div>
+              ))}
+          </div>
         </div>
       )}
     </PageTemplate>
