c29e072681aba06ebdfa9e47f8a7b5f194d4cdc5
jmakhmudov <makhmudov@tuta.io>
2024-02-16 00:58:49 +0500

    change apps names

--- server/AuctionBot/notifications/apps.py
+++ server/AuctionBot/notifications/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class NotificationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notifications'
+    verbose_name = 'Анонсы'
 
     def ready(self):
         import notifications.signals
