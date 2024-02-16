ce025ed93b2bfe75cfc2fe26dc0da12ab8063f89
EdemKemalov <edemkelohesoyam@gmail.com>
2024-02-17 01:53:20 +0500

    notification-feature 1.2

--- server/AuctionBot/notifications/views.py
+++ server/AuctionBot/notifications/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def your_view(request):
+    can_view_custom_notif_button = request.user.has_perm('notifications.can_view_custom_notif_button')
+    return render(request, 'base.html', {'can_view_custom_notif_button': can_view_custom_notif_button})
