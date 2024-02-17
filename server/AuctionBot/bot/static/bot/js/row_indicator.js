0e761f18dc22c04e6d9110ba529845700ef28e19
jmakhmudov <makhmudov@tuta.io>
2024-02-17 16:52:15 +0500

    fix bug

--- server/AuctionBot/bot/static/bot/js/row_indicator.js
+++ server/AuctionBot/bot/static/bot/js/row_indicator.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         var currentElement = row;
         while (currentElement && currentElement.tagName !== 'TABLE') {
-            if (img.alt === 'True') {
+            if (img.alt === 'True' && currentElement.tagName === 'TR') {
                 currentElement.style.backgroundColor = 'rgba(161, 255, 165, 0.15)';
             } else if (img.alt === 'False' && currentElement.tagName === 'TR') {
                 currentElement.style.backgroundColor = 'rgba(255, 146, 146, 0.15)';
