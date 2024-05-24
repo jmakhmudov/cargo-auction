914bcb9d80bb0a6dbe91bc958fea0af7e36e6cb6
jmakhmudov <makhmudov@tuta.io>
2024-05-24 23:43:00 +0500

    vite config

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,10 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
   server: {
     proxy: {
       '/api': 'http://77.91.70.108:8000/'
-    }
+    },
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
   },
-  plugins: [react()],
 })
